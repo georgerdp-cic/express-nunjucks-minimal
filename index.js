961260d95b402e6c45231da18e8cc85fa513efc6
georgerdp-cic <george.crisan-cic@ibm.com>
2021-06-23 15:11:40 +0100

    [FEATURE] Example of passing data

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const app = express();
 
 nunjucks.configure(path.resolve(__dirname, 'templates') ,{
     autoescape: true,
-    express: app
+    express: app,
+    watch: true
  });
 
 app.set("view engine", "njk");
 
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {
+        pageTitle: 'Welcome test page',
+        headerBodyText: 'This is header body text',
+        featList: [
+            {
+                name: 'test',
+                desc: 'does this and that'
+            },            {
+                name: 'test2',
+                desc: 'does this and that'
+            },
+            {
+                name: 'test3',
+                desc: 'does this and that'
+            }
+        ]
+    });
 });
 
 app.listen(3000, () => {
