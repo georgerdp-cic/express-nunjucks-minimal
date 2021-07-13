6e905bd7be9e10a2be528c81bb993f01bd0dfb74
georgerdp-cic <george.crisan-cic@ibm.com>
2021-07-13 15:40:40 +0100

    [FEATURE] Demo accordion

--- src/routes/demoform.ts
+++ src/routes/demoform.ts
@@ -2,14 +2,22 @@ import { Router } from 'express';
 import csurf from 'csurf';
 const router = Router();
 
-const csrfP = csurf({ cookie: true });
+//const csrfP = csurf({ cookie: true });
 
 router.get('/demo-form', function (req, res) {
     
     res.render('views/demoform', {
-        //cToken: req.csrfToken(),
         pageTitle: 'demo-form',
         tabName: 'demoform'
+    });
+
+});
+
+router.get('/accordion', function (req, res) {
+    
+    res.render('views/demoaccordion', {
+        pageTitle: 'demo-accordion',
+        tabName: 'demoaccordion'
     });
 
 });
